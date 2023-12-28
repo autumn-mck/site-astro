f50f8f7c4b52bffc8e067bc78c87494375593d63
Autumn McKee <autumn@mck.is>
2023-12-28 23:05:33 +0000

    Update index

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,4 @@
 export const SITE_TITLE = "James' small space on the web";
-export const SITE_DESCRIPTION = 'Text description here';
+export const SITE_DESCRIPTION = "Text description here";
+export const SITE_DOMAIN = "mck.is"
+
