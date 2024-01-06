4fc53cf5b0fe583c02c2eb7e351164ddef58a095
Autumn McKee <autumn@mck.is>
2024-01-06 17:15:31 +0000

    Significantly improved mobile support

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "💻 James' small space on the web";
+export const SITE_TITLE_SHORT = "💻 James' site";
 export const SITE_DESCRIPTION = "Text description here";
 export const SITE_DOMAIN = "mck.is";
