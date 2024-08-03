9833fcf0e2ddffb3aa9bedd1a9ece70ac3ce2d7d
Autumn McKee <autumn@mck.is>
2024-08-03 14:42:43 +0100

    add options to cat

--- src/components/terminal/commands.ts
+++ src/components/terminal/commands.ts
@@ -200,7 +200,70 @@ async function env(env: envType) {
 	return 0;
 }
 
-async function cat(env: envType, file: string) {
+async function cat(env: envType, ...args: string[]) {
+	const availableOptions = [
+		{ name: "help", long: "--help", short: "", takesArg: false },
+		{ name: "version", long: "--version", short: "", takesArg: false },
+		{
+			name: "numberNonBlank",
+			long: "--number-nonblank",
+			short: "-b",
+			takesArg: false,
+		},
+		{ name: "showEnds", long: "--show-ends", short: "-E", takesArg: false },
+		{ name: "numberLines", long: "--number", short: "-n", takesArg: false },
+		{
+			name: "squeezeBlank",
+			long: "--squeeze-blank",
+			short: "-s",
+			takesArg: false,
+		},
+		{ name: "ignored", long: "", short: "-u", takesArg: false }, // easiest feature to implement
+	] as optionType[];
+
+	const options = await checkOptionsFromArgs(args, availableOptions, "cat");
+
+	if (!options) return 1;
+
+	if (options.help) {
+		printTermLine(`Usage: cat [OPTION]... [FILE]...
+Concatenate FILE(s) to standard output.
+
+  -b, --number-nonblank    number nonempty output lines, overrides -n
+  -E, --show-ends          display $ at end of each line
+  -n, --number             number all output lines
+  -s, --squeeze-blank      suppress repeated empty output lines
+  -u                       (ignored)
+      --help        display this help and exit
+      --version     output version information and exit`);
+		return 0;
+	}
+
+	if (options.version) {
+		printTermLine(`cat (jsh) 0.1.0`);
+		return 0;
+	}
+
+	const files = args.filter((arg) => !arg.startsWith("-"));
+	if (files.length === 0) {
+		printTermLine(
+			"cat: No files specified\n(will handle reading from stdin in the future)"
+		);
+		return 1;
+	}
+
+	for (const file of files) {
+		let exitCode = await catFile(options, file);
+		if (exitCode !== 0) return exitCode;
+	}
+
+	return 0;
+}
+
+async function catFile(
+	options: Record<string, string | boolean>,
+	file: string
+) {
 	const imageExtensions = ["jpg", "jpeg", "png", "gif", "webp"];
 	const fullFilePath = tryParsePath(file);
 
@@ -217,20 +280,58 @@ async function cat(env: envType, file: string) {
 	}
 
 	if (typeof obj === "function") {
-		printTermLine(obj.toString());
+		await catText(obj.toString(), options);
 	} else if (typeof obj === "string") {
 		const extenstion = file.split(".")?.pop()!;
 
 		if (imageExtensions.includes(extenstion)) {
 			printImage(obj);
 		} else {
-			printTermLine(obj);
+			await catText(obj, options);
 		}
 	} else {
 		printTermLine("File type not supported!");
 	}
 
 	return 0;
+}
+
+async function catText(
+	text: string,
+	options: Record<string, string | boolean>
+) {
+	let lineNum = 1;
+	let lastLineWasBlank = false;
+
+	let textToPrint = [] as string[];
+
+	for (let line of text.split("\n")) {
+		if (options.squeezeBlank && lastLineWasBlank && line.trim() === "") {
+			continue;
+		}
+
+		lastLineWasBlank = line.trim() === "";
+
+		if (options.numberNonBlank) {
+			if (line.trim() === "") {
+				textToPrint.push(line);
+			} else {
+				line = `  ${lineNum} ${line}`;
+				lineNum++;
+			}
+		} else if (options.numberLines) {
+			line = `  ${lineNum} ${line}`;
+			lineNum++;
+		}
+
+		if (options.showEnds) {
+			line += "$";
+		}
+
+		textToPrint.push(line);
+	}
+
+	printTermLine(textToPrint.join("\n"));
 }
 
 async function cd(env: envType, path: string) {
