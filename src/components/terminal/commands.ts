9ef19bb5221a26133ceab42df086f05668556e73
Autumn McKee <autumn@mck.is>
2024-08-04 22:41:00 +0100

    (wip) tree options

--- src/components/terminal/commands.ts
+++ src/components/terminal/commands.ts
@@ -468,15 +468,58 @@ Options:
 	return 0;
 }
 
-async function tree(env: envType, path: string | undefined) {
+async function tree(env: envType, ...args: string[]) {
+	const availableOptions = [
+		{ name: "help", long: "--help", short: "", takesArg: false },
+		{ name: "version", long: "--version", short: "", takesArg: false },
+		// listing options
+		{ name: "dirsOnly", long: "", short: "-d", takesArg: false },
+		{ name: "fullPathPrefix", long: "", short: "-f", takesArg: false },
+		{ name: "levelLimit", long: "", short: "-L", takesArg: true },
+		{ name: "rerunOnMaxDepth", long: "", short: "-R", takesArg: false },
+		{ name: "pruneEmptyDirs", long: "--prune", short: "", takesArg: false },
+		// file options
+		{ name: "quoteFileNames", long: "", short: "-Q", takesArg: false },
+		// XML/JSON options
+		{ name: "xml", long: "", short: "-X", takesArg: false },
+		{ name: "json", long: "", short: "-J", takesArg: false },
+	] as optionType[];
+
+	const parsedParts = await checkOptionsFromArgs(
+		args,
+		availableOptions,
+		"tree"
+	);
+
+	if (!parsedParts) return 2;
+
+	console.log(parsedParts);
+
+	const options = parsedParts.options;
+	args = parsedParts.nonOptionArgs;
+
+	if (options.help) {
+		printTermLine(`Usage: tree [OPTION]... [DIRECTORY]...`);
+		return 0;
+	}
+
+	if (options.version) {
+		printTermLine(`tree (jsh) 0.1.0`);
+		return 0;
+	}
+
+	let path = args[0];
 	path = tryParsePath(path);
 
-	printTermLine(`.${await treeDir(path)}`);
+	printTermLine(`.${await treeDir(path, options)}`);
 
 	return 0;
 }
 
-async function treeDir(path: string) {
+async function treeDir(
+	path: string,
+	options: Record<string, string | boolean>
+) {
 	path = tryParsePath(path);
 	const obj = getObjAtPath(path);
 	if (!obj || typeof obj !== "object") return "";
@@ -485,15 +528,23 @@ async function treeDir(path: string) {
 
 	let tree = "";
 	for (let i = 0; i < items.length; i++) {
-		const item = items[i];
+		let item = items[i];
+		if (options.dirsOnly && typeof obj[item] !== "object") continue;
 		const isLast = i === items.length - 1;
 		const connector = isLast ? "└──" : "├──";
-		tree += `\n${connector} ${item}`;
+		const pathPrefix = (options.fullPathPrefix ? `${path}/` : "").replace(
+			"//",
+			"/"
+		);
+		tree += `\n${connector} ${pathPrefix}${item}`;
 
 		if (typeof obj[item] === "object" && Object.keys(obj[item]).length > 0) {
 			const newPath = `${path}/${item}`;
 			const isLastDir = i === items.length - 1;
-			const indented = await indentTree(await treeDir(newPath), isLastDir);
+			const indented = await indentTree(
+				await treeDir(newPath, options),
+				isLastDir
+			);
 			tree += `\n${indented}`;
 		}
 	}
