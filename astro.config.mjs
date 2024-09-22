6571701e1e578a8bf04eabdab622cf4ffd63dfef
Autumn McKee <autumn@mck.is>
2024-09-22 19:22:11 +0100

    prettify output html

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import { compile } from "@catppuccin/vscode";
 
+import htmlBeautifier from "astro-html-beautifier";
+
 let vscodeCatppuccinMacchiato = compile("macchiato");
 vscodeCatppuccinMacchiato.colors["editor.background"] = "#181926";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://mck.is",
-	integrations: [sitemap()],
+	compressHTML: false,
+	integrations: [sitemap(), htmlBeautifier()],
+	build: {
+		inlineStylesheets: "never",
+	},
 	markdown: {
 		shikiConfig: {
 			theme: vscodeCatppuccinMacchiato,
