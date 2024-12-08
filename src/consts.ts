f4ef294eff8bfc589d0b0602732315396add938a
Autumn McKee <autumn@mck.is>
2024-12-08 18:13:24 +0000

    read name from config

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,7 @@
-export const SITE_TITLE = "💻 James' small space on the web";
-export const SITE_TITLE_SHORT = "💻 James' site";
-export const SITE_DESCRIPTION = "Text description here";
+export const NAME = "James McKee";
+const firstName = NAME.split(" ")[0];
+
+export const SITE_TITLE = `💻 ${firstName}'s small space on the web`;
+export const SITE_TITLE_SHORT = `💻 ${firstName}'s site`;
+
 export const SITE_DOMAIN = "mck.is";
