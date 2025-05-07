bb30c474453923e1b8424a2e5b0295602cfc57e1
Autumn McKee <autumn@mck.is>
2025-05-07 22:46:26 +0100

    maybe fix rss images

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,30 +1,83 @@
-import rss from "@astrojs/rss";
+import rss, { type RSSFeedItem } from "@astrojs/rss";
 import { getCollection } from "astro:content";
 import { compareByPublishDate } from "./blog.astro";
 
 import sanitizeHtml from "sanitize-html";
 import MarkdownIt from "markdown-it";
-const parser = new MarkdownIt();
+import { parse as htmlParser } from "node-html-parser";
+import type { AstroGlobal, ImageMetadata } from "astro";
+import { getImage } from "astro:assets";
+const markdownParser = new MarkdownIt();
 
-export async function GET(context: { site: string }) {
+const imagesGlob = import.meta.glob<{ default: ImageMetadata }>(
+	"/src/content/**/*.{jpeg,jpg,png,gif,avif,webp,svg}" // add more image formats if needed
+);
+
+export async function GET(context: AstroGlobal) {
 	const posts = (await getCollection("markdownPosts"))
 		.sort((a, b) => compareByPublishDate(a, b))
 		.filter((post) => post.data.published)
 		.filter((post) => post.data.public);
 
-	return rss({
-		title: "weird autumn's blog",
-		description: "who up xml-ing they rss",
-		site: context.site,
-		items: posts.map((post) => ({
+	const feed: RSSFeedItem[] = [];
+
+	for (const post of posts) {
+		const body = markdownParser.render(post.body);
+		const html = htmlParser.parse(body);
+		const images = html.querySelectorAll("img");
+
+		for (const img of images) {
+			const src = img.getAttribute("src")!;
+
+			// Relative paths that are optimized by Astro build
+			if (src.startsWith("/images")) {
+				// images starting with `/images/` is the public dir
+				img.setAttribute("src", context.site + src.replace("/", ""));
+			} else if (src.startsWith("./") || true) {
+				// remove prefix of `./`
+				const prefixRemoved = src.replace("./", "");
+				// create prefix absolute path from root dir
+				const imagePathPrefix = `/src/content/markdownPosts/${post.slug}/${prefixRemoved}`;
+				console.log("imagePathPrefix", imagePathPrefix);
+
+				// call the dynamic import and return the module
+				const imagePath = await imagesGlob[imagePathPrefix]?.()?.then(
+					(res) => res.default
+				);
+
+				console.log("imagePath", imagePath);
+
+				if (imagePath) {
+					const optimizedImg = await getImage({ src: imagePath });
+					// set the correct path to the optimized image
+					img.setAttribute(
+						"src",
+						context.site + optimizedImg.src.replace("/", "")
+					);
+				}
+			} else {
+				console.error("src unknown", src);
+				throw Error("src unknown");
+			}
+		}
+
+		feed.push({
 			title: post.data.title,
 			pubDate: post.data.published,
 			description: post.data.description,
-			content: sanitizeHtml(parser.render(post.body), {
+			link: `/${post.slug}`,
+			// sanitize the new html string with corrected image paths
+			content: sanitizeHtml(html.toString(), {
 				allowedTags: sanitizeHtml.defaults.allowedTags.concat(["img"]),
 			}),
-			link: `/${post.slug}/`,
-		})),
+		});
+	}
+
+	return rss({
+		title: "weird autumn's blog",
+		description: "who up xml-ing they rss",
+		site: context.site!,
+		items: feed,
 		customData: `<language>en-gb</language>`,
 	});
 }
